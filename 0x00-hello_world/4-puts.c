4db7c8924692a0432eeeb2c0a61ea3b2a457d25f
Mohamed AJJAJI <majjaji98@gmail.com>
2023-03-16 10:49:58 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Use the function puts
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
