0a6daab7ea4ce02b2cc8b232ee286cedac16909f
Mohamed AJJAJI <majjaji98@gmail.com>
2023-04-17 19:21:17 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog owner's name
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
