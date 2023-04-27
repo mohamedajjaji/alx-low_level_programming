d179234dd41ad746a53e3efd9459b3514826c289
Mohamed AJJAJI <majjaji98@gmail.com>
2023-04-27 16:29:58 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * first - prints a message before the main function is executed
+ *
+ * Return: void
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
